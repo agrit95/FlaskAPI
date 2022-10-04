afddd0fb952a3964e2470e677bf62de91e37b6cf
Agrit Tyagi <agrittyagi95@gmail.com>
2022-10-04 17:47:44 +0530

    Changed the app.py, uwsgi.ini & run.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '9jxpe7jrw8has9'
 api = Api(app)
@@ -23,6 +24,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
-    db.create_all(app=app)
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
