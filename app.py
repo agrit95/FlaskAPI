cd0e2bb4cc8f90ae5b956950f1e724058d3f2e1b
Agrit Tyagi <agrittyagi95@gmail.com>
2022-10-04 17:40:48 +0530

    Changed the app.py, uwsgi.ini & deleted run.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -22,12 +23,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
+    db.create_all(app=app)
     app.run(port=5000)
