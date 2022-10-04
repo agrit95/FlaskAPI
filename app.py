7467f7ebc86b2c756358c894c9ae6a5bef908ff5
Agrit Tyagi <agrittyagi95@gmail.com>
2022-10-04 19:33:31 +0530

    modified app.py & uwsgi.ini file & added run.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 
@@ -21,11 +20,6 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all(app=app)
-
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
@@ -33,8 +27,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    db.init_app(app)
-
     # if app.config['DEBUG']:
     #     @app.before_first_request
     #     def create_tables():
