2ffa7ef6e24d8080fff665e0817f709ba87cb203
Agrit Tyagi <agrittyagi95@gmail.com>
2022-10-16 01:51:22 +0530

    Made changes to app.py

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    @app.before_first_request
     def create_tables():
         db.create_all()
     app.run(port=5000, debug=True)
