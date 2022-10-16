1335dc8cfab511fa3b9e5f2e24b7bcc5e6b1c26e
Agrit Tyagi <agrittyagi95@gmail.com>
2022-10-16 14:23:52 +0530

    added @jwt_required(optional=True) for /items

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
 from models.item import ItemModel
 
 
@@ -50,6 +50,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-
+    @jwt_required(optional=True)
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data will be available if you login'
+        }
